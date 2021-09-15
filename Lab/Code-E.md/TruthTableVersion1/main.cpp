d4c1a8928e7d5e707b069841cbfdb9dc6c8310ca
scottrenfrow01 <77753517+scottrenfrow01@users.noreply.github.com>
2021-09-14 17:16:29 -0700

    Create main.cpp

---
+++ Lab/Code-E.md/TruthTableVersion1/main.cpp
@@ -0,0 +1,53 @@
+/* 
+ * File:   main.cpp
+ * Author: Scott Renfrow
+ * Created on:  September 14, 2021, 4:43PM
+ * Purpose:  To tell the Truth
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    bool x,y;
+    
+    //Display the Heading
+    cout<<"X Y !X !Y X&&Y X||Y X^Y X^Y^X X^Y^Y !(X&&Y) !X||!Y "<<
+          " !(X||Y) !X&&!Y"<<endl;
+    
+    while (cin>>x && cin>>y){
+    
+    cout<<(x?'T':'F')<<" "
+        <<(y?'T':'F')<<"  "
+        <<(!x?'T':'F')<<"  "
+        <<(!y?'T':'F')<<"   "
+        <<(x&&y?'T':'F')<<"    "
+        <<(x||y?'T':'F')<<"   "
+        <<(x^y?'T':'F')<<"    "
+        <<(x^y^x?'T':'F')<<"     "
+        <<(x^y^y?'T':'F')<<"       "
+        <<(!(x&&y)?'T':'F')<<"      "
+        <<(!x||!y?'T':'F')<<"        "
+        <<(!(x||y)?'T':'F')<<"      "
+        <<(!x&&!y?'T':'F')
+        <<endl;
+    
+    
+
+    }
+        
+    cout << endl;
+
+    //Exit
+    return 0;
+}
