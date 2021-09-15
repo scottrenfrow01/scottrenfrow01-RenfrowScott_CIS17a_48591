e2cf611998042dc4d26caa5fd089c3682d40c644
scottrenfrow01 <77753517+scottrenfrow01@users.noreply.github.com>
2021-09-14 17:14:13 -0700

    Create main.cpp

---
+++ Lab/Code-E.md/CodeE_HelloWorld/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * Author: Scott Renfrow
+ * Date: Created on: September 14, 2021, 4:45 PM
+ * Purpose: Ouput Hello Wolrd
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Constants like PI, e, Gravity, Conversions, 2D array size only!
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Set Random Number Seed Here
+    
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    cout << endl << "   Hello World!  " << endl << endl;
+
+    //Exit
+    return 0;
+}
